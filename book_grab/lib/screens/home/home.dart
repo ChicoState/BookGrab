e5c53aee3d2317652b0f04745a0dc30bbe268c96
Joshua Meadows <jmeadows4@mail.csuchico.edu>
2020-03-10 16:07:30 -0700

    added a simple snapshot example from firebase

--- book_grab/lib/screens/home/home.dart
+++ book_grab/lib/screens/home/home.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:book_grab/services/auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 //going to wrap our book list widget and settings form
 //this will be a stateless widget
 class Home extends StatelessWidget {
 
   final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -18,28 +20,41 @@ class Home extends StatelessWidget {
           elevation: 0.0,
           actions: <Widget>[
             FlatButton.icon(
-//logout button will call signout on our authservice.
+            //logout button will call signout on our authservice.
               icon: Icon(Icons.person),
               label: Text('logout'),
               onPressed: () async {
-
                 await _auth.signOut();
               },
 
-
             ),
-
-
-
           ],
-
-
         ),
+        //Builds a stream of user snapshots, and creates a list
+        //of users emails to display on the home page. Just an example
+        //of how we can access fields from the database.
+        body: StreamBuilder(
+            stream: Firestore.instance.collection('users').snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return const Text("Loading...");
+              return ListView.builder(
+                  itemExtent: 80.0,
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot ds = snapshot.data.documents[index];
+                    return new Row(
+                      textDirection: TextDirection.ltr,
+                      children: <Widget>[
+                        Expanded(child: Text(ds["email"])),
+                      ],
 
+                    );
+                  }
 
-
+              );
+            }
+        ),
       ),
     );
-
   }
 }
