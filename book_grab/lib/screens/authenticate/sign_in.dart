217bc736b06eb1c9e1c9bfe4646894d88a77ddf0
kbains6 <kbains6@mail.csuchico.edu>
2020-03-23 16:20:05 -0700

    UI improvements to homescreen and added asset folder for images

--- book_grab/lib/screens/authenticate/sign_in.dart
+++ book_grab/lib/screens/authenticate/sign_in.dart
@@ -2,11 +2,10 @@ import 'package:book_grab/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
-
 //property to be accessed from authenticate.dart for toggleview
 
   final Function toggleView;
-  SignIn({ this.toggleView });
+  SignIn({this.toggleView});
 
   @override
   _SignInState createState() => _SignInState();
@@ -22,113 +21,224 @@ class _SignInState extends State<SignIn> {
   String password = ' ';
   String error = '';
 
-
   //key will be used to identify form
   final _formKey = GlobalKey<FormState>();
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[100],
-      appBar: AppBar(
-        backgroundColor: Colors.green[400],
-        elevation: 0.0,
-        title: Text('Sign in to bookgrab'),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text('Register'),
-            //function that switches us from one screen to another
-            onPressed: (){
-              //refer to the widget and call toggleview onpressed
-              widget.toggleView();
-            }
-          )
-
-
-        ],
-
-
-      ),
-
-
+      resizeToAvoidBottomPadding: false,
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
-
+        //padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Color.fromRGBO(150, 0, 0, .8),
         child: Form(
           //associate global key with our form's validation.
           key: _formKey,
-          child: Column(
+          child: Stack(
             children: <Widget>[
-              SizedBox(height: 5.0),
-              //text field for email with onchanged prop val is contents of formfield, every time value in formfield changes this runs
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'email',
+              Align(
+                alignment: Alignment.bottomRight,
+                widthFactor: 0.3,
+                heightFactor: 0.3,
+                child: Material(
+                  borderRadius: BorderRadius.all(Radius.circular(200)),
+                  color: Color.fromRGBO(100, 0, 0, 9),
+                  child: Container(
+                    width: 400,
+                    height: 400,
+                  ),
                 ),
-                onChanged: (val) {
-                setState(()=> email = val);
-
-                }
               ),
-              //sized box for spacing
-              SizedBox(height: 5.0),
-              //password form field, obscure text
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'password',
+              Center(
+                child: Container(
+                  width: 450,
+                  height: 450,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Material(
+                          elevation: 10.0,
+                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              'assets/wild_logo.png',
+                              width: 80,
+                              height: 80,
+                            ),
+                          )),
+
+                      // Title text for Book Grab
+                      Container(
+                        width: 150,
+                        child: Text(
+                          'Book Grab',
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+
+                      // Input fields for username and password
+
+//text field for email with onchanged prop val is contents of formfield, every time value in formfield changes this runs
+
+                      Container(
+                        width: 250,
+                        child: Material(
+                            elevation: 5.0,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            color: Color.fromRGBO(139, 0, 0, 0.4),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child:
+                                      Icon(Icons.person, color: Colors.white),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.only(
+                                        topRight: Radius.circular(10.0),
+                                        bottomRight: Radius.circular(10.0)),
+                                  ),
+                                  width: 200,
+                                  height: 60,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: TextFormField(
+                                        decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            hintText: 'email',
+                                            fillColor: Colors.white,
+                                            filled: true),
+                                        style: TextStyle(
+                                            fontSize: 20.0,
+                                            color: Colors.black),
+                                        onChanged: (val) {
+                                          setState(() => email = val);
+                                        }),
+                                  ),
+                                ),
+                              ],
+                            )),
+                      ),
+                      //sized box for spacing
+                      SizedBox(height: 5.0),
+                      Container(
+                        width: 250,
+                        child: Material(
+                            elevation: 5.0,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            color: Color.fromRGBO(139, 0, 0, 0.4),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Icon(Icons.lock, color: Colors.white),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.only(
+                                        topRight: Radius.circular(10.0),
+                                        bottomRight: Radius.circular(10.0)),
+                                  ),
+                                  width: 200,
+                                  height: 60,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: TextFormField(
+                                        decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            hintText: 'password',
+                                            fillColor: Colors.white,
+                                            filled: true),
+                                        style: TextStyle(
+                                            fontSize: 20.0,
+                                            color: Colors.black),
+                                        onChanged: (val) {
+                                          setState(() => password = val);
+                                        }),
+                                  ),
+                                ),
+                              ],
+                            )),
+                      ),
+
+                      // login button eventually will be a form
+                      Container(
+                        width: 150,
+                        child: RaisedButton(
+                          onPressed: () async {
+                            //printing email password combo for debug purposes
+                            //print(email);
+                            //print(password);
+                            if (_formKey.currentState.validate()) {
+                              //signin and gather result for this
+                              dynamic result = await _auth
+                                  .signinWithEmailPassword(email, password);
+                              if (result == null) {
+                                setState(() => error =
+                                    'Couldnt signin with provided info');
+                              }
+                            }
+                          },
+                          color: Color.fromRGBO(139, 0, 0, 1),
+                          textColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10.0))),
+                          child: Text(
+                            'Login',
+                            style: TextStyle(fontSize: 20.0),
+                          ),
+                        ),
+                      ),
+                      //display error
+                      SizedBox(height: 8.0),
+                      Text(
+                        error,
+                        style: TextStyle(color: Colors.white, fontSize: 10.0),
+                      ),
+
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 20.0, horizontal: 50.0),
+                        child: RaisedButton(
+                            color: Color.fromRGBO(139, 0, 0, 1),
+                            textColor: Colors.white,
+                            child: Text('Register',
+                                style: TextStyle(fontSize: 20.0)),
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10.0))),
+                            onPressed: () {
+                              //refer to the widget and call toggleview onpressed
+                              widget.toggleView();
+                            }),
+                      ),
+
+                      Text(
+                        'New to BookGrab? Then Register above!',
+                        style: TextStyle(color: Colors.white, fontSize: 10.0),
+                      ),
+                    ],
+                  ),
                 ),
-                onChanged: (val){
-                setState(()=> password = val);
-
-                }
-
               ),
-              SizedBox(height: 5.0),
-              RaisedButton(
-                color: Colors.green[500],
-                child: Text(
-                   'Sign in',
-                    style: TextStyle(color: Colors.white),
-                    //need a style to change color of text
-
-
-              ),
-                  //Asynchronous function as we want to go out and interact with firebase to sign the user in. (so it will take time, thus async)
-                  onPressed: () async {
-                   //printing email password combo for debug purposes
-                    //print(email);
-                    //print(password);
-                    if (_formKey.currentState.validate()){
-                      //signin and gather result for this
-                      dynamic result = await _auth.signinWithEmailPassword(email, password);
-                     if (result == null){
-                       setState(() => error = 'Couldnt signin with provided info');
-                     }
-                    }
-                  }
-
-
-                ),
-              //display error
-              SizedBox(height: 8.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 10.0),
-              ),
-
             ],
-
-
           ),
-
-
         ),
-
       ),
-
     );
   }
 }
